66e2030a6c4f0044022a9d95bf62888945993a02
Matt Dodge <matt@neutral.io>
2014-10-07 17:50:38 -0700

    Getting stock action going

---
+++ README.md
@@ -0,0 +1,42 @@
+Stock
+============
+
+Returns some stock quotes, along with other information, from Yahoo Finance
+
+Properies
+---------
+
+-   **symbols**: List of stock tickers to get quotes for
+
+Dependencies
+------------
+None
+
+Commands
+--------
+None
+
+Input
+-----
+N/A
+
+Output
+------
+A list of signals, one per ticker. 
+
+The following are fields that are returned in each signal:
+
+- DaysHigh
+- YearHigh
+- Name
+- StockExchange
+- MarketCapitalization
+- Volume
+- DaysLow
+- YearLow
+- DaysRange
+- LastTradePriceOnly
+- Change
+- symbol
+- Symbol
+- AverageDailyVolume
---
+++ stock_block.py
@@ -0,0 +1,41 @@
+from nio.metadata.properties import ListProperty
+from nio.common.discovery import Discoverable, DiscoverableType
+from nio.common.signal.base import Signal
+from .http_blocks.rest.rest_block import RESTPolling
+
+
+@Discoverable(DiscoverableType.block)
+class Stock(RESTPolling):
+
+    _yql_base_url = ("https://query.yahooapis.com/v1/public/yql?"
+                     "q=select%20*%20from%20yahoo.finance.quote%20where"
+                     "%20symbol%20in%20({0})&format=json"
+                     "&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+
+    symbols = ListProperty(str, title='Symbols/Tickers')
+    queries = None
+
+    def __init__(self):
+        super().__init__()
+
+    def _prepare_url(self, paging=False):
+        sym_str = ",".join(['"' + sym + '"' for sym in self.symbols])
+        self._url = self._yql_base_url.format(sym_str)
+
+        return {"Content-Type": "application/json"}
+
+    def _process_response(self, resp):
+        body = resp.json()
+
+        try:
+            results = body['query']['results']
+        except Exception:
+            self._logger.error("Invalid resposnse format: {0}".format(body))
+            results = None
+
+        if not results or not isinstance(results, dict):
+            self._logger.warning("No results found: {0}".format(results))
+            return [], None
+
+        # No paging
+        return [Signal(s) for s in results.get('quote', [])], None
